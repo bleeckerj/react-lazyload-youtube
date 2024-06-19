967a435d84e4d8c705e496981010885a0f7faaa9
Julian Bleecker <julian@nearfuturelaboratory.com>
2024-06-19 08:27:12 -0700

    Added the ability to add additional parameters to the YouTube URL for such things as starting timecode, etc.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 import styles from './styles.module.css'
 
 const Youtube = (props) => {
-  const { videoId, width, height, imgSize } = props
+  const { videoId, width, height, imgSize, addParams } = props
   const [showVideo, setShowVideo] = useState(false)
+  
+  const getSrcUrl = () => {
+    let url = `https://www.youtube.com/embed/${videoId}?autoplay=1&showinfo=0`;
+    if (addParams) {
+      url += `&${addParams}`;
+    }
+    return url;
+  };
   return (
     <React.Fragment>
       {showVideo ? (
         <iframe
           width={width || '100%'}
           height={height || '400px'}
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&showinfo=0`}
+          src={getSrcUrl()}
           frameBorder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
           allowFullScreen
